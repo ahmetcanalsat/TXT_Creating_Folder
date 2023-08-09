79e40f7b244c3d9964b06af3d7284ac3c05c663a
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-08-09 16:12:44 +0300

    update create_folder
    HER HAFTA İÇİN 30 GÜNLÜK .txt DOSYASI OLUŞTURUYOR KONTROL EDİLECEK.

--- create_folder_v0.2.py
+++ create_folder_v0.2.py
@@ -4,7 +4,7 @@ for j in range(1, 7):
     os.makedirs("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j), mode=0o755, exist_ok=True)
     for i in range(0, 30):
         i += 1
-        j += 1
+
         if (i > 0 or i <= 5):
             dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
