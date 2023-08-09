eb3aa1c89f1dc294fd1535fd38fcdf446e9bdc28
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-08-09 15:21:12 +0300

    update create_folder
    Klasörlerin içine  .txt dosyalarını atabiliyorum fakat dosyaların hepsini hafta hafta bölmek yerine her hafta için 30 tane .txt dosyası oluşturuyor. Çalışıyor ama TEKRAR KONTROL EDİLECEK.

--- create_folder_v0.2.py
+++ create_folder_v0.2.py
@@ -1,27 +1,29 @@
 import os
 
 for j in range(1, 7):
-    ana_klasor=os.makedirs("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j), mode=0o755, exist_ok=True)
-    j += 1
+    os.makedirs("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j), mode=0o755, exist_ok=True)
     for i in range(0, 30):
         i += 1
-        if (i > 0 and i <= 5):
-            dosyad = open("C:/Users/HP/Desktop/python_cikti/Staj Defteri Gün_" + str(i) + ".txt", "w")
+        j += 1
+        if (i > 0 or i <= 5):
+            dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
-        elif (i > 5 and i <= 10):
-            dosyad = open("C:/Users/HP/Desktop/python_cikti/Staj Defteri Gün_" + str(i) + ".txt", "w")
+        elif (i > 5 or i <= 10):
+            dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
-        elif (i > 10 and i <= 15):
-            dosyad = open("C:/Users/HP/Desktop/python_cikti/Staj Defteri Gün_" + str(i) + ".txt", "w")
+        elif (i > 10 or i <= 15):
+            dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
-        elif (i > 15 and i <= 20):
-            dosyad = open("C:/Users/HP/Desktop/python_cikti/Staj Defteri Gün_" + str(i) + ".txt", "w")
+        elif (i > 15 or i <= 20):
+            dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
         elif (i > 20 and i <= 25):
-            dosyad = open("C:/Users/HP/Desktop/python_cikti/Staj Defteri Gün_" + str(i) + ".txt", "w")
+            dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
         elif (i > 25 and i <= 30):
-            dosyad = open("C:/Users/HP/Desktop/python_cikti/Staj Defteri Gün_" + str(i) + ".txt", "w")
+            dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
         else:
             "Stajınız bitmiştir."
+
+    j += 1
