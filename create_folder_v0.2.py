5b20fb3d8c6f4f93e3d382cd66429fe85b2a8b69
Ahmet Can ALSAT <ahmetcan_045@hotmail.com>
2023-08-10 16:45:47 +0300

    update create_folder
    if koşulundaki or komutları and olarak güncellendi

--- create_folder_v0.2.py
+++ create_folder_v0.2.py
@@ -4,16 +4,16 @@ for j in range(1, 7):
     os.makedirs("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j), mode=0o755, exist_ok=True)
     for i in range(0, 30):
         i += 1
-        if (i > 0 or i <= 5):
+        if (i > 0 and i <= 5):
             dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
-        elif (i > 5 or i <= 10):
+        elif (i > 5 and i <= 10):
             dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
-        elif (i > 10 or i <= 15):
+        elif (i > 10 and i <= 15):
             dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
-        elif (i > 15 or i <= 20):
+        elif (i > 15 and i <= 20):
             dosyad = open("C:/Users/HP/Desktop/python_cikti/Hafta-" + str(j) + "/Staj Defteri Gün_" + str(i) + ".txt","w")
 
         elif (i > 20 and i <= 25):
